e8ab7140dc7f1f0ac684c5b6c0d4218aa1e652ad
SamaraiKai <samuraininjakai@gmail.com>
2026-07-03 01:02:35 -0400

    Configure Next.js with TypeScript settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    devIndicators: false,
+    typescript: {
+        ignoreBuildErrors: true,
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
